af9f5ec45a7f3db5744fb90b6ff9fae44ac8607e
Fredrick Aponyo <fraponyo94@gmail.com>
2018-06-08 13:02:53 +0300

    project structure and basic configuration

---
+++ src/main/java/team/project/dairymanagementsystem/DairymanagementsystemApplication.java
@@ -0,0 +1,12 @@
+package team.project.dairymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairymanagementsystemApplication.class, args);
+	}
+}
