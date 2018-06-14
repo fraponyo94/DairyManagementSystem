204dac772c73c1608254124d68c211afdbc8318f
OyugiTolbert <oyugitolbert@gmail.com>
2018-06-14 14:56:57 +0300

    user details UI

--- src/main/java/team/project/dairymanagementsystem/DairymanagementsystemApplication.java
+++ src/main/java/team/project/dairymanagementsystem/DairymanagementsystemApplication.java
@@ -2,11 +2,20 @@ package team.project.dairymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class DairymanagementsystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DairymanagementsystemApplication.class, args);
+
 	}
-}
+
+
+	}
+
