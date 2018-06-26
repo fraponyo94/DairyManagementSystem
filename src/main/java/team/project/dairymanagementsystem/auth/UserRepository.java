aa2c88fb48747208b3bb35ce269c19e1307de616
EricGachoka <gachokaeric819@gmail.com>
2018-06-26 01:19:07 -0700

    deleted auth directory

--- src/main/java/team/project/dairymanagementsystem/auth/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package team.project.dairymanagementsystem.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.project.dairymanagementsystem.domain.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    User findByNationalId(int id);
-}
-
