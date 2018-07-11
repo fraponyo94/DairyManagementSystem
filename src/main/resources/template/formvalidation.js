8e89a9e1b806c11c5d37aaf2cb260ed940865e7b
OyugiTolbert <oyugitolbert@gmail.com>
2018-07-11 14:55:25 +0300

    corected

--- src/main/resources/template/formvalidation.js
+++ src/main/resources/template/formvalidation.js
@@ -1,9 +1,8 @@
 function validateform() {
     var X = document.getElementById("fname");
 
-    if(X.value == ""){
+    if(X.value == "tolbert"){
         Alert("please enter correct name");
         return false;
     }
-
 }
