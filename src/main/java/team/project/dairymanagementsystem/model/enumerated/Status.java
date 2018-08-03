54f9b861cac9236e7fdd3aea2a7e2d7e3ecaa53e
EricGachoka <gachokaeric819@gmail.com>
2018-08-03 03:26:46 -0700

    adds javadoc comments to classes in enumerated package

--- src/main/java/team/project/dairymanagementsystem/model/enumerated/Status.java
+++ src/main/java/team/project/dairymanagementsystem/model/enumerated/Status.java
@@ -1,7 +1,12 @@
 package team.project.dairymanagementsystem.model.enumerated;
 
+/**
+ * An enum for the various stages a contract application can be in
+ * PENDING - new applications
+ * APPROVED - approved applications that were previously pending
+ * DENIED - denied applications that were previously pending
+ * CANCELLED - cancelled applications that were previously approved
+ */
 public enum Status {
-
     PENDING, APPROVED, DENIED, CANCELLED;
-
 }
