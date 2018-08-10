343a1d6efeead612841f241dbb8d8e19d5f61145
OyugiTolbert <oyugitolbert@gmail.com>
2018-08-10 16:34:46 +0300

    cheched

---
+++ target/classes/template/formvalidation.js
@@ -0,0 +1,12 @@
+function validateform() {
+    var X = document.getElementById("fname");
+
+    if(X.value == "tolbert"){
+        Alert("please enter correct name");
+        return false;
+    }
+}
+
+function alerting(){
+    alert("would like to continue with registration");
+}
